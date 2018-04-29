db4b13a6a9a6ed43e662c5d263ea201ea8aa1ca7
riv4wi <riv4wi@gmail.com>
2018-04-29 02:31:25 -0300

    Adapt boot method in file AuthServiceProvider.php, according to authentication requirements. In this case verify that there is a http header called api-token with the correct value for that user can authenticate

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -31,9 +31,9 @@ class AuthServiceProvider extends ServiceProvider
         // the User instance via an API token or any other method necessary.
 
         $this->app['auth']->viaRequest('api', function ($request) {
-            $api_token = $request->header('api_token');
-            if ($api_token && $api_token == 'freedom') {
-                return new User();
+            $api_token = $request->header('api-token'); // IMPORTANT: Name to header must be spinal-case
+            if ($api_token && $api_token=='freedom'){
+                return new User(); // This is didactic
             }
             return null;
         });
